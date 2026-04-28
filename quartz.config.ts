151fa1aa81f0929f3455fc6ae97431aad11927e9
sifca-akahori <akahori@sifca.com>
2026-04-28 11:53:15 +0900

    Set up GitHub Pages deployment

--- quartz.config.ts
+++ quartz.config.ts
@@ -16,7 +16,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "quartz.jzhao.xyz",
+    baseUrl: "sifca-akahori.github.io/ob",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "modified",
     theme: {
@@ -88,8 +88,8 @@ const config: QuartzConfig = {
       Plugin.Static(),
       Plugin.Favicon(),
       Plugin.NotFoundPage(),
-      // Comment out CustomOgImages to speed up build time
-      Plugin.CustomOgImages(),
+      // Disabled to keep local builds independent from external font/image fetches.
+      // Plugin.CustomOgImages(),
     ],
   },
 }
